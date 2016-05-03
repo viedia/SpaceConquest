de5dc0ccae3af9fbba8202480d64b171351d56cd
viedia <laetitia.lemiere@gmail.com>
2016-05-03 13:10:24 +0200

    ajout de commentaire à tourDesZombie

--- src/spaceconquest/TimerPartie.java
+++ src/spaceconquest/TimerPartie.java
@@ -61,31 +61,31 @@ public class TimerPartie extends Timer {
     
         public void tourDesZombies() {
             
-            trans(partie.getCarte().getGrapheZombie());
-            Couple posDep= this.partie.getLicoShip().getPosition();
-            Couple posArr = this.partie.getZombificator().getPosition();
-            Couple dessin;
+            trans(partie.getCarte().getGrapheZombie()); //transpose le graphe des zombies
+            Couple posDep= this.partie.getLicoShip().getPosition(); //récupère la position des licornes = position de départ car le graphe est transposé
+            Couple posArr = this.partie.getZombificator().getPosition(); //récupère la position des zombies = position de d'arrivée car le graphe est transposé
+            Couple dessin; // case à colorier en jaune
 
             int sommetZ;
-            int sommetArr= sommetZ = this.partie.getCarte().coupleToSommet(posArr);
+            int sommetArr= sommetZ = this.partie.getCarte().coupleToSommet(posArr); // traduit en sommet les couple posDep et posArr
             int sommetDep = this.partie.getCarte().coupleToSommet(posDep);
-            
+             //j'effectue Dijkstra
             Dijktra dij = new Dijktra (partie.getCarte().getGrapheZombie());
             dij.CalculDistance(sommetDep);
-           
+           // récupère le prédesseceur de la case du Zombie
             int pred = dij.getPi()[sommetArr];
-            
+            // si le licorne et les zombie ne se suivent pas = ne sont pas côte à côte
             if (dij.getPi()[sommetZ]!=sommetDep) {
-                sommetArr = pred;
-                dessin = this.partie.getCarte().sommetToCouple(pred,this.partie.getCarte().getTaille());
-                this.partie.getCarte().coloreCaseJaune(dessin.getX(), dessin.getY());
-                pred = dij.getPi()[sommetArr]; 
+                sommetArr = pred; // je recule d'une case
+                dessin = this.partie.getCarte().sommetToCouple(pred,this.partie.getCarte().getTaille()); //récupère la case à colorier
+                this.partie.getCarte().coloreCaseJaune(dessin.getX(), dessin.getY());// je colorie la premère case 
+                pred = dij.getPi()[sommetArr]; // récupère le pred du prédecesseur des zombies
             }
-            this.partie.getCarte().coloreCaseJaune(posArr.getX(), posArr.getY());
-            posDep = this.partie.getCarte().sommetToCouple(pred,this.partie.getCarte().getTaille()); 
+            this.partie.getCarte().coloreCaseJaune(posArr.getX(), posArr.getY());//colorie
+            posDep = this.partie.getCarte().sommetToCouple(pred,this.partie.getCarte().getTaille()); //converti en Couple
             
-            this.partie.getCarte().BougerVaisseau(posArr, posDep );           
-            this.partie.getZombificator().setPosition(posDep); 
+            this.partie.getCarte().BougerVaisseau(posArr, posDep );    //déplace le vaisseau        
+            this.partie.getZombificator().setPosition(posDep); // met à jour la position du vaisseau zombie
         }
             
         public void trans(Graphe g){
